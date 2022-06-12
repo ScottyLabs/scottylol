1a28fc3290d40b9f06ccc3c75ed5ab2d8e181d76
Anuda Weerasinghe <anuda@live.com>
2022-06-12 01:07:43 -0700

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express from 'express';
 import fs from 'fs';
+import yaml from 'yaml';
 
 const app = express();
+const COMMAND_DIR = './commands';
+
+const readCommands = () => {
+  const commandDir = fs.readdirSync(COMMAND_DIR);
+  const result: {[string: any]} = {};
+  for (const filename of commandDir) {
+    const file = fs.readFileSync(COMMAND_DIR + '/' + filename).toString();
+    const fileYaml = yaml.parse(file);
+    
+    for (const command of fileYaml['matches']) {
+      result[command] = {
+        
+      }
+    }
+  }
+}
 
 (async () => {
-  const commandDir = fs.readdirSync('./commands');
-  console.log(commandDir);
+  readCommands();
 })();
