03c40a0729c5fd68af709a9669cddf0107fb29ff
Anuda Weerasinghe <anuda@live.com>
2022-06-12 02:41:43 -0700

    implements basic redirects with text 404 and closest match

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,41 @@
 import express from 'express';
 import fs from 'fs';
 import yaml from 'yaml';
+import { closestMatch } from 'closest-match';
 
-const app = express();
 const COMMAND_DIR = './commands';
+const PORT = 3000;
 
 type CommandConfig = {
   name: string;
   description: string;
   author: string;
   matches: string[];
-  redirect: string;
-  param?: string;
+  searchUrl?: string;
+  home: string;
 }
 
-type CommandMapping = {[key: string]: string};
+type Redirect = {
+  searchUrl?: string;
+  home: string
+}
 
-const readCommands = () => {
+type CommandMapping = {[key: string]: Redirect};
+
+const readCommands = (): CommandMapping => {
   const commandDir = fs.readdirSync(COMMAND_DIR);
   const result: CommandMapping = {};
   for (const filename of commandDir) {
     const file = fs.readFileSync(COMMAND_DIR + '/' + filename).toString();
     const fileYaml: CommandConfig = yaml.parse(file);
-    const redirectEndpoint = fileYaml.redirect;
-    const queryParam = fileYaml.param;
-    const redirectUrl = queryParam !== undefined ? `${redirectEndpoint}?${queryParam}=` : `${redirectEndpoint}/`;
+    const searchUrl = fileYaml.searchUrl;
+    const home = fileYaml.home;
 
     try {
-      new URL(redirectUrl + 'test');
+      if (searchUrl) {
+        new URL(searchUrl + 'test');
+      }
+      new URL(home);
     } catch (e) {
       console.error(e);
       console.log(`${fileYaml.name} in ${filename} has an invalid URL`);
@@ -39,7 +47,8 @@ const readCommands = () => {
         console.log("${command} already declared!");
         continue;
       }
-      result[command] = redirectUrl;
+      const redirect: Redirect = {searchUrl, home};
+      result[command] = redirect;
     }
   }
   return result;
@@ -47,5 +56,39 @@ const readCommands = () => {
 
 (async () => {
   const mappings = readCommands();
-  console.log(mappings);
+  const app = express();
+  
+  app.get('/search', (req, res, next) => {
+    const { q } = req.query;
+    if (typeof q !== "string") {
+      return res.status(400).send("Bad request");
+    }
+    const match = q.match(/^(\S*)(?:\s(.*))?$/);
+    if (match === null) {
+      return res.status(400).send("Bad request");
+    }
+    const token = match[1];
+    const query = match[2];
+    const config = mappings[token];
+    const home = config.home;
+    const searchUrl = config.searchUrl;
+
+    // If no matching token is found go to fallback
+    if (!(token in mappings)) {
+      const closest = closestMatch(token, Object.keys(mappings));
+      return next({token, closest});
+    }
+
+    const target = (searchUrl === undefined || query === undefined) ? home : searchUrl + query;
+
+    const url = new URL(target);
+    res.redirect(url.toString());
+  });
+
+  const errorHandler: express.ErrorRequestHandler = (value: {token: string, closest: string}, req, res, next) => {
+    res.status(404).send(`Command not found: ${value.token}. Did you mean: ${value.closest}?`);
+  }
+  app.use(errorHandler);
+
+  app.listen(PORT, () => console.log(`Server is listening at port ${PORT}.`));
 })();
