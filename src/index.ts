b6672d5623b652b4449c6f6267b150d7ae9e8ca0
Anuda Weerasinghe <anuda@live.com>
2022-06-12 01:30:38 -0700

    implements initial command parsing functionality

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,47 @@ import yaml from 'yaml';
 const app = express();
 const COMMAND_DIR = './commands';
 
+type CommandConfig = {
+  name: string;
+  description: string;
+  author: string;
+  matches: string[];
+  redirect: string;
+  param?: string;
+}
+
+type CommandMapping = {[key: string]: string};
+
 const readCommands = () => {
   const commandDir = fs.readdirSync(COMMAND_DIR);
-  const result: {[string: any]} = {};
+  const result: CommandMapping = {};
   for (const filename of commandDir) {
     const file = fs.readFileSync(COMMAND_DIR + '/' + filename).toString();
-    const fileYaml = yaml.parse(file);
-    
+    const fileYaml: CommandConfig = yaml.parse(file);
+    const redirectEndpoint = fileYaml.redirect;
+    const queryParam = fileYaml.param;
+    const redirectUrl = queryParam !== undefined ? `${redirectEndpoint}?${queryParam}=` : `${redirectEndpoint}/`;
+
+    try {
+      new URL(redirectUrl + 'test');
+    } catch (e) {
+      console.error(e);
+      console.log(`${fileYaml.name} in ${filename} has an invalid URL`);
+      continue;
+    }
+
     for (const command of fileYaml['matches']) {
-      result[command] = {
-        
+      if (command in result) {
+        console.log("${command} already declared!");
+        continue;
       }
+      result[command] = redirectUrl;
     }
   }
+  return result;
 }
 
 (async () => {
-  readCommands();
+  const mappings = readCommands();
+  console.log(mappings);
 })();
