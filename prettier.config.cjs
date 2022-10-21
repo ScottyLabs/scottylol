024aecbb0780685ac8b3e036b965c054bf90a33a
Adrastopoulos <gabriel.hall@cox.net>
2022-10-21 11:53:28 -0400

    Add prettier configuration

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.cjs',
+  trailingComma: 'none',
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true
+};
