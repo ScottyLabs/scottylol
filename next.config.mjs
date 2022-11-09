0e345d1a168fb02f8e9236f07d52bf68d25c9e41
David Ethan Hwang <hdavidethan@gmail.com>
2022-11-08 22:21:36 -0500

    Add raw-loader to load yaml files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({ test: /\.ya?ml$/, use: 'raw-loader' });
+
+    return config;
+  }
 };
 
 export default nextConfig;
