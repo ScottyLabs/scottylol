f1b28402d1978bbd4828919b6b427b4ab8cf0c00
Andrew923 <yuandrew923@gmail.com>
2022-11-19 14:27:02 -0500

    check netlify

--- src/pages/noob.tsx
+++ src/pages/noob.tsx
@@ -9,7 +9,7 @@ export const getServerSideProps: GetServerSideProps<{}> = async (ctx) => {
   const { mapping } = readCommands();
   const { url } = ctx.req;
   if (typeof url !== 'string') return { notFound: true };
-  const q = decodeURIComponent(url.replace(/^[^_]*=/, ''));
+  const q = url.replace(/^[^_]*=/, '').replaceAll('=', '');
   const match = q.match(/^(\S*)(?:\s(.*))?$/);
   if (match === null) return { notFound: true };
   const token = match[1].toLowerCase();
